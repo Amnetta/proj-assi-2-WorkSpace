b44d794893ff1b678b7ff0795971682427974692
Amnetta <118825211+Amnetta@users.noreply.github.com>
2023-01-31 10:30:06 +0100

    intial commit

---
+++ backend/src/test/java/se/jensenyh/javacourse/saltmerch/backend/Tests.java
@@ -0,0 +1,29 @@
+package se.jensenyh.javacourse.saltmerch.backend;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import se.jensenyh.javacourse.saltmerch.backend.controller.ProductController;
+import se.jensenyh.javacourse.saltmerch.backend.repository.ProductRepository;
+import se.jensenyh.javacourse.saltmerch.backend.service.ProductService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+public class Tests {
+    @Autowired
+    ProductController productController;
+
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(productController).isNotNull();
+
+
+        productController.getProducts();
+    }
+
+}
+
